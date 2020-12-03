90c1c8af89655ca729f825b4025afa38e9422c8d
Alexander Colen <alexanderc@live.nl>
2020-12-03 11:01:45 -0500

    Day 3 Puzzle 2

--- Day03/DayThree.cs
+++ Day03/DayThree.cs
@@ -18,40 +18,55 @@ namespace AdventOfCode2020.Day03
 
         public void Puzzle1()
         {
-            var solution = 0;
-            var last = 0;
-            int step;
-            // Remove first row to skip.
-            grid.Remove(grid[0]);
-            foreach (var y in grid)
-            {
-                step = last + 3;
-                if (step >= y.Length)
-                {
-                    step -= y.Length;
-                }
-                for (int x = 0; x < y.Length; x++)
-                {
-                    if (x == step)
-                    {
-                        if (y[x] == '#')
-                        {
-                            solution++;
-                        }
-                        last = x;
-                        break;
-                    }
-                }
-            }
+            var solution = CheckSlopes(3, 1);
 
             Console.WriteLine($"Puzzle 1 solution: {solution}");
         }
 
         public void Puzzle2()
         {
-            var solution = 0;
+            long solution = CheckSlopes(1, 1) * CheckSlopes(3, 1) * CheckSlopes(5, 1) * CheckSlopes(7, 1) * CheckSlopes(1, 2);
 
             Console.WriteLine($"Puzzle 2 solution: {solution}");
+        }
+
+        private long CheckSlopes(int step_x, int step_y)
+        {
+            var solution = 0;
+            var last_x = 0;
+            var last_y = 0;
+            int step;
+            for (var y = 0; y < grid.Count; y++)
+            {
+                if (y < last_y + step_y)
+                {
+                    continue;
+                } else
+                {
+                    last_y = y;
+                }
+
+                // Calculate next x position to check.
+                step = last_x + step_x;
+                if (step >= grid[y].Length)
+                {
+                    step -= grid[y].Length;
+                }
+                for (int x = 0; x < grid[y].Length; x++)
+                {
+                    if (x == step)
+                    {
+                        if (grid[y][x] == '#')
+                        {
+                            solution++;
+                        }
+                        last_x = x;
+                        break;
+                    }
+                }
+            }
+
+            return solution;
         }
 
         public void ReadInput()
