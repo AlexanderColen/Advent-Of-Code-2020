70cf6c4c9c3a1a49e65d92f250364b001d2f38ca
Alexander Colen <alexanderc@live.nl>
2020-12-14 14:31:00 -0500

    Day 14 Puzzle 1

--- Day14/DayFourteen.cs
+++ Day14/DayFourteen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020.Day14
@@ -19,62 +20,38 @@ namespace AdventOfCode2020.Day14
 
         public void Puzzle1()
         {
-            ulong solution = 0;
-            var defaultMask = "".PadLeft(64, '0').ToCharArray();
+            var mask = "0";
 
             foreach (var instruction in instructions)
             {
-                // Overwrite bits in mask.
+                // Change mask.
                 if (instruction.StartsWith("mask"))
                 {
-                    var bits = instruction[7..].PadLeft(64, '0');
-                    // Overwrite default mask.
-                    for (var i = 0; i < bits.Length; i++)
-                    {
-                        if (bits[i] != 'X')
-                        {
-                            defaultMask[i] = bits[i];
-                        }
-                    }
-
-                    var newMemory = new Dictionary<int, string>();
-                    // Overwrite in address.
-                    foreach (var address in memory.Keys)
-                    {
-                        var mask = memory[address].ToCharArray();
-                        for (var i = 0; i < bits.Length; i++)
-                        {
-                            if (bits[i] != 'X')
-                            {
-                                mask[i] = bits[i];
-                            }
-                        }
-                        newMemory.Add(address, string.Join("", mask));
-                    }
-                    memory = newMemory;
+                    mask = instruction[7..].PadLeft(64, 'X');
                 }
                 // Write value to memory.
                 else if (instruction.StartsWith("mem"))
                 {
                     var matches = new Regex(@"\d+").Matches(instruction);
                     var address = int.Parse(matches[0].Value);
-                    var value = matches[1].Value;
-                    // Add default mask to address if it's no initialized yet.
-                    if (!memory.ContainsKey(address))
-                    {
-                        memory.Add(address, string.Join("", defaultMask));
-                    }
+                    var value = int.Parse(matches[1].Value);
 
-                    memory[address] = Convert.ToString(Convert.ToInt64(memory[address], 2) + long.Parse(value), 2).PadLeft(64, '0');
+                    // Set binary value.
+                    memory[address] = Convert.ToString(value, 2).PadLeft(64, '0');
+                    // Override with mask.
+                    var newValue = memory[address].ToCharArray();
+                    for (var i = mask.Length - 1; i > -1; i--)
+                    {
+                        if (mask[i] != 'X')
+                        {
+                            newValue[i] = mask[i];
+                        }
+                    }
+                    memory[address] = string.Join("", newValue);
                 }
             }
 
-            // Calculate total.
-            foreach (var address in memory.Keys)
-            {
-                Console.WriteLine($"Address: {address} - Raw: {memory[address]} - Value: {Convert.ToUInt64(memory[address], 2)}");
-                solution += Convert.ToUInt64(memory[address], 2);
-            }
+            var solution = memory.Select(m => Convert.ToUInt64(m.Value, 2)).Aggregate((a, b) => a + b);
 
             Console.WriteLine($"Puzzle 1 solution: {solution}");
         }
