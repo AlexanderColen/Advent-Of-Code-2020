2847579d6d0622045a09fbd4a893ad9e4e3611bb
Alexander Colen <alexanderc@live.nl>
2020-12-19 12:05:42 -0500

    Day 19 Puzzle 2

--- Day19/DayNineteen.cs
+++ Day19/DayNineteen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020.Day19
@@ -37,6 +38,28 @@ namespace AdventOfCode2020.Day19
         public void Puzzle2()
         {
             var solution = 0;
+
+            // Edit rule 8 into 8: 42 | 42 8 and rule 11 into 11: 42 31 | 42 11 31
+            // 42 | 42 8 (replacing 8 with this is just a string of 42s)
+            rules[8] = " 42 +";
+            // 42 31 | 42 11 31 (Potentially infinite repeats of 42 followed by 42 x times and 31 x times followed by 31.)
+            rules[11] = " 42 31 ";
+            // 5 repeats is the magic number for my input.
+            var repeats = 5;
+            for (var i = 1; i < repeats; i++)
+            {
+                rules[11] = $"{rules[11]}|{string.Concat(Enumerable.Repeat(" 42 ", i))}{string.Concat(Enumerable.Repeat(" 31 ", i))}";
+            }
+
+            var zero = rules[0];
+            var re = new Regex(@"\d+");
+            var outcome = SubstituteRules(zero, re).Replace(" ", "");
+
+            var re2 = new Regex($"^{outcome}$");
+            foreach (var message in messages)
+            {
+                solution += re2.IsMatch(message) ? 1 : 0;
+            }
 
             Console.WriteLine($"Puzzle 2 solution: {solution}");
         }
